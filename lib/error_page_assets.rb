da49451436c41083e7c42d0a70d41e599fc02cbd
Scott Bronson <brons_git@rinspin.com>
2015-03-26 01:50:25 -0700

    add a railtie so rake usage is automatic

---
+++ lib/error_page_assets.rb
@@ -0,0 +1,3 @@
+module ErrorPageAssets
+  require 'error_page_assets/railtie' if defined?(Rails)
+end
---
+++ lib/error_page_assets/railtie.rb
@@ -0,0 +1,13 @@
+require 'error_page_Assets'
+require 'rails'
+
+module ErrorPageAssets
+  class Railtie < Rails::Railtie
+    railtie_name :error_page_assets
+
+    rake_tasks do
+      load "tasks/error_page_assets.rake"
+    end
+  end
+end
+
